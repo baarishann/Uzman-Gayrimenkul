33e172023ee867cab2505f3865a1eb93688b1eaf
baarishann <baarishann@gmail.com>
2026-08-29 02:57:31 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,18 @@
-const CACHE='ug-v2';const ASSETS=['./','index.html','style.css','app.js','manifest.json','icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="ug-v3";
+
+self.addEventListener("install",()=>{
+ self.skipWaiting();
+});
+
+self.addEventListener("activate",e=>{
+ e.waitUntil(
+  caches.keys().then(x=>
+   Promise.all(x.map(k=>caches.delete(k)))
+  )
+ );
+ self.clients.claim();
+});
+
+self.addEventListener("fetch",e=>{
+ e.respondWith(fetch(e.request));
+});

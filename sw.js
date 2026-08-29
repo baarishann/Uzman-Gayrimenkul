89aaef6ffcf43dce02b142831b3f6f806a315d8f
baarishann <baarishann@gmail.com>
2026-08-29 03:11:38 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,1 @@
-const CACHE="ug-v3";
 
-self.addEventListener("install",()=>{
- self.skipWaiting();
-});
-
-self.addEventListener("activate",e=>{
- e.waitUntil(
-  caches.keys().then(x=>
-   Promise.all(x.map(k=>caches.delete(k)))
-  )
- );
- self.clients.claim();
-});
-
-self.addEventListener("fetch",e=>{
- e.respondWith(fetch(e.request));
-});

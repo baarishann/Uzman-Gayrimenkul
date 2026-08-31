d8b67fd4b45e46183a9567f7c82d091b9b6bb344
baarishann <baarishann@gmail.com>
2026-08-31 05:41:48 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='uzman-emlak-v43-theme';
-const CORE=['./','./index.html','./style.css?v=43','./app.js?v=43','./manifest.json?v=43','./icon-192.png','./icon-512.png','./uzman-emlak-kapak.png','./CNAME'];
+const CACHE='uzman-emlak-v44';
+const CORE=['./','./index.html','./style.css?v=44','./app.js?v=44','./manifest.json?v=44','./icon-192.png','./icon-512.png','./icon-1024.png','./uzman-emlak-kapak.png','./CNAME'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
